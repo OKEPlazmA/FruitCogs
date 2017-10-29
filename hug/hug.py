7d6e9e31117b1a7ca9fcb0081d8454a556a66e5b
OKEPlazmA <okeplazma@gmail.com>
2017-10-29 16:14:12 -0500

    Update hug.py

--- hug/hug.py
+++ hug/hug.py
@@ -12,7 +12,7 @@ class Hug:
         author = context.message.author.mention
         mention = member.mention
         
-        hug = "**{0} gave {1} a kiss!**"
+        hug = "**{0} hugs {1}!**"
         
         choices = ['http://i.imgur.com/sW3RvRN.gif', 'http://i.imgur.com/gdE2w1x.gif', 'http://i.imgur.com/zpbtWVE.gif', 'http://i.imgur.com/ZQivdm1.gif', 'http://i.imgur.com/MWZUMNX.gif', 'https://m.popkey.co/fca5d5/bXDgV.gif', 'http://gifimage.net/wp-content/uploads/2017/01/Anime-hug-GIF-Image-Download-24.gif', 'https://i.imgur.com/2WywS3T.gif']
         
