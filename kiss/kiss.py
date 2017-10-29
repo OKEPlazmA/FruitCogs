604c3542f1cc25ab7d10c28c44514e6127536f53
OKEPlazmA <okeplazma@gmail.com>
2017-10-29 15:59:43 -0500

    Update kiss.py

--- kiss/kiss.py
+++ kiss/kiss.py
@@ -12,31 +12,13 @@ class Kiss:
         author = context.message.author.mention
         mention = member.mention
         
-        hug = "**{0} kissed {1}!**"
+        kiss = "**{0} gave {1} a kiss!**"
         
-        choices = [
-    "https://i.imgur.com/II1bakc.gif",
-    "https://i.imgur.com/MzAjNdv.gif",
-    "https://i.imgur.com/eKcWCgS.gif",
-    "https://i.imgur.com/3aX4Qq2.gif",
-    "https://i.imgur.com/uobBW9K.gif",
-    "https://i.imgur.com/3jzT5g6.gif",
-    "https://i.imgur.com/Esqtd1u.gif",
-    "https://i.imgur.com/FozOXkB.gif",
-    "https://i.imgur.com/7GhTplD.gif",
-    "https://i.imgur.com/B6UKulT.gif",
-    "https://i.imgur.com/Uow8no2.gif",
-    "https://i.imgur.com/EwQPLZI.gif",
-    "https://i.imgur.com/agdhkfE.gif",
-    "https://i.imgur.com/1IuyOxK.gif",
-    "https://i.imgur.com/nodEB4W.gif",
-    "https://i.imgur.com/KSiA3Ws.gif",
-    "https://i.imgur.com/nVM7Ll8.gif",
-    "https://i.imgur.com/glTzPMZ.gif"]
+        choices = ['http://i.imgur.com/0D0Mijk.gif', 'http://i.imgur.com/TNhivqs.gif', 'http://i.imgur.com/3wv088f.gif', 'http://i.imgur.com/7mkRzr1.gif', 'http://i.imgur.com/8fEyFHe.gif', "https://i.imgur.com/II1bakc.gif", "https://i.imgur.com/MzAjNdv.gif", "https://i.imgur.com/eKcWCgS.gif", "https://i.imgur.com/3aX4Qq2.gif", "https://i.imgur.com/uobBW9K.gif", "https://i.imgur.com/3jzT5g6.gif", "https://i.imgur.com/Esqtd1u.gif", "https://i.imgur.com/FozOXkB.gif", "https://i.imgur.com/7GhTplD.gif", "https://i.imgur.com/B6UKulT.gif", "https://i.imgur.com/Uow8no2.gif", "https://i.imgur.com/EwQPLZI.gif", "https://i.imgur.com/agdhkfE.gif", "https://i.imgur.com/1IuyOxK.gif", "https://i.imgur.com/nodEB4W.gif", "https://i.imgur.com/KSiA3Ws.gif", "https://i.imgur.com/nVM7Ll8.gif", "https://i.imgur.com/glTzPMZ.gif"]
         
         image = random.choice(choices)
         
-        embed = discord.Embed(description=hug.format(author, mention), colour=0xffa500())
+        embed = discord.Embed(description=kiss.format(author, mention), colour=discord.Colour.blue())
         embed.set_image(url=image)
 
         await self.bot.say(embed=embed)
